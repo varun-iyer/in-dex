a035d7a97f7ecae752d6105c8c5a021eef9a636e
Varun Iyer <varun_iyer@posteo.net>
2022-06-02 16:45:11 -0700

    Finished in_dex function

--- in_dex.py
+++ in_dex.py
@@ -5,8 +5,10 @@ from stop_words import get_stop_words
 import re
 
 stop_words = get_stop_words('en')
+legal_stop_words = ["u.s.", "v."]
+stop_words += legal_stop_words
 
-word = re.compile("\w+")
+word = re.compile("[a-zA-Z]+")
 
 test_sentences = [
     "This is an awesome book to learn NLP.",
@@ -15,12 +17,12 @@ test_sentences = [
 ]
 
 def tokdoc(raw_text):
-    raw_text = raw_text.replace("\n", " ")
+    raw_text = raw_text.replace("\n", " ").lower()
     tok = sent_tokenize(raw_text)
     tok = [[w for w in word_tokenize(sentence) if (w not in stop_words and re.match(word, w))] for sentence in tok]
     return tok
 
-katz = tokdoc(open("katz.txt").read())
+katz = open("katz.txt").read().replace("U. S.", "U.S.")
 
 def nwise(iterable, n=2):
     """
@@ -28,34 +30,37 @@ def nwise(iterable, n=2):
 
     for 'ABCDEFG', nwise('ABCDEFG', 3) -> 'ABC', 'BCD', 'CDE'...
     """
-    return zip(*[tokenized_sentences[i::n] for i in range(n)])
+    return zip(*[iterable[i::n] for i in range(n)])
 
 def ngram_tfidf(tokenized_sentences, sentence_ngram=1, word_ngram=1):
     df = {}
     tf = {}
     for sentences in nwise(tokenized_sentences, sentence_ngram):
-        document = sum(sentences)
+        document = sum(sentences, [])
         words = [" ".join(w) for w in nwise(document, word_ngram)]
         wordcount = {}
         for w in words:
             wordcount[w] = wordcount.get(w, 0) + 1
         for word, occurences in wordcount.items():
             tf[word] = tf.get(word, 0) + occurences
-            df[word] = tf.get(word, 0) + 1
-    n_documents = sentences + 1 - sentence_ngram
+            df[word] = df.get(word, 0) + 1
+    n_documents = len(sentences) + 1 - sentence_ngram
     idf = {k: n_documents / v for k, v in df.items() if v > 1}
-    average_tf = {k: v / df[k] for k, v in tf.items() if v / df[k] > 1}
-    keys = set().intersection(idf.keys(), average_tf.keys())
+    average_tf = {k: v / df[k] for k, v in tf.items() if v > 1}
+    keys = set.intersection(set(idf.keys()), set(average_tf.keys()))
     return {key: idf[key] * average_tf[key] for key in keys}
  
-def helpful_links(sentences, method: ("sent2vec", "tfidf")="sent2vec"):
-    graph = cosine_graph(sentences, skip_adjacent=10)
-    ordered = sorted(graph.items(), key=lambda x: x[1])[::-1]
-    n = int(len(sentences)/10)
-    return ordered[:n]
+def fuzzy_search(term, text):
+    return list(re.finditer(r"\s.{,15}".join(term), text.lower()))
 
+def in_dex(document):
+    tok = tokdoc(document)
+    sorted_phrases = sum([list(ngram_tfidf(tok, 4, n).keys()) for n in range(4, 1, -1)], [])
+    n_sentence = len(tok)
+    sorted_phrases = sorted_phrases[:n_sentence//5]
+    link_sets = [fuzzy_search(sp.split(" "), document) for sp in sorted_phrases]
+    return link_sets
 
 # see https://github.com/plangrid/pdf-annotate
 if __name__ == "__main__":
-    # k = helpful_links(katz)
-    ngram_tfidf(katz)
+    k = in_dex(katz)
