270424b8771397470054b35a6700bfb99073ff49
Varun Iyer <varun_iyer@posteo.net>
2022-06-02 22:13:35 -0700

    Html arrow rendering

--- in_dex.py
+++ in_dex.py
@@ -3,8 +3,9 @@ from scipy.spatial.distance import cosine
 from nltk.tokenize import sent_tokenize, word_tokenize
 from stop_words import get_stop_words
 import re
+from bs4 import BeautifulSoup
 
-stop_words = get_stop_words('en')
+stop_words = get_stop_words("en")
 legal_stop_words = ["u.s.", "v."]
 stop_words += legal_stop_words
 
@@ -16,13 +17,23 @@ test_sentences = [
     "We can interchangeably use embedding, encoding, or vectorizing.",
 ]
 
+
 def tokdoc(raw_text):
     raw_text = raw_text.replace("\n", " ").lower()
     tok = sent_tokenize(raw_text)
-    tok = [[w for w in word_tokenize(sentence) if (w not in stop_words and re.match(word, w))] for sentence in tok]
+    tok = [
+        [
+            w
+            for w in word_tokenize(sentence)
+            if (w not in stop_words and re.match(word, w))
+        ]
+        for sentence in tok
+    ]
     return tok
 
+
 katz = open("katz.txt").read().replace("U. S.", "U.S.")
+
 
 def nwise(iterable, n=2):
     """
@@ -31,6 +42,7 @@ def nwise(iterable, n=2):
     for 'ABCDEFG', nwise('ABCDEFG', 3) -> 'ABC', 'BCD', 'CDE'...
     """
     return zip(*[iterable[i::n] for i in range(n)])
+
 
 def ngram_tfidf(tokenized_sentences, sentence_ngram=1, word_ngram=1):
     df = {}
@@ -44,28 +56,86 @@ def ngram_tfidf(tokenized_sentences, sentence_ngram=1, word_ngram=1):
         for word, occurences in wordcount.items():
             tf[word] = tf.get(word, 0) + occurences
             df[word] = df.get(word, 0) + 1
-    n_documents = len(sentences) + 1 - sentence_ngram
+    n_documents = len(tokenized_sentences) + 1 - sentence_ngram
     idf = {k: n_documents / v for k, v in df.items() if v > 1}
     average_tf = {k: v / df[k] for k, v in tf.items() if v > 1}
     keys = set.intersection(set(idf.keys()), set(average_tf.keys()))
     return {key: idf[key] * average_tf[key] for key in keys}
- 
+
+
 def fuzzy_search(term, text):
     return list(re.finditer(r".{,3}\s(\w+.{,3}\s){,5}".join(term), text.lower()))
 
+
 def key_phrases(document):
     tok = tokdoc(document)
-    sorted_phrases = sum([list(ngram_tfidf(tok, 4, n).keys()) for n in range(4, 1, -1)], [])
+    sorted_phrases = sum(
+        [list(ngram_tfidf(tok, 4, n).keys()) for n in range(4, 1, -1)], []
+    )
     n_sentence = len(tok)
-    sorted_phrases = sorted_phrases[:n_sentence//5]
+    sorted_phrases = sorted_phrases[: n_sentence // 5]
     return sorted_phrases
 
-def in_dex(document):
-    sorted_phrases = key_phrases(document)
+
+def in_dex(document, sorted_phrases=None):
+    if not sorted_phrases:
+        sorted_phrases = key_phrases(document)
     link_sets = [fuzzy_search(sp.split(" "), document) for sp in sorted_phrases]
     return link_sets
 
-# see https://github.com/plangrid/pdf-annotate
+
+def html_wrap(html, span: (0, -1), tag: ("<a>", "</a>")):
+    """
+    Wrap a given span of html with a tag, returning the number of characters
+    added.
+    """
+    out_html = (
+        html[: span[0]]
+        + tag[0]
+        + html[span[0] : span[1]]
+        + tag[1]
+        + html[span[1] :]
+    )
+    return out_html
+
+
+def in_dex_html(html_file):
+    bs = BeautifulSoup(open(html_file))
+    out_html = str(bs.html)
+    indices = in_dex(out_html, key_phrases(bs.text))
+    annotations = []
+    for nset, match_set in enumerate(indices):
+        for nmatch, match in enumerate(match_set):
+            annotations.append((nset, nmatch, match))
+    annotations = sorted(annotations, key=lambda x: match.span()[0])
+
+    for nset, nmatch, match in annotations:
+            start = match.span()[0]
+            while rematch := re.search(str(match.group()), out_html[start:], re.IGNORECASE):
+                if out_html[start + rematch.span()[0] - 1] != ">" and \
+                        out_html[start + rematch.span()[1]] != ">":
+                    break
+                start += len(rematch.group())
+
+            if not rematch:
+                print("No rematch")
+                continue
+            link_up = ""
+            link_down = ""
+            if nmatch - 1 >= 0:
+                link_up = f'<a href="#{nset}_{max(nmatch-1, 0)}"><sup>&uarr;</sup></a>'
+            if nmatch + 1 <= len(indices[nset]):
+                link_down = f'<a href="#{nset}_{min(nmatch+1, len(indices[nset])-1)}" class="noline"><sup>&darr;</sup></a>'
+            out_html = html_wrap(
+                out_html,
+                [start + r for r in rematch.span()],
+                (f'<a id="{nset}_{nmatch}">', f'</a>' + link_up + link_down)
+            )
+    outfile = open(f"{html_file}.idx", "w")
+    from css import css
+    outfile.write(f"<!DOCTYPE html> <html><head><style> {css} </style></head>")
+    outfile.write(out_html)
+    return out_html
+
 if __name__ == "__main__":
-    k = in_dex(katz)
-     
+    in_dex_html("katz.html")
